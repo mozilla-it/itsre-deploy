25a19f9e9debdfd41064caca7e2889afb883fe79
limed <limed@mozilla.com>
2019-03-22 11:42:50 -0700

    Remove some password policy options

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -3,12 +3,10 @@ resource "aws_iam_account_alias" "alias" {
 }
 
 resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 16
-  max_password_age               = 90
-  password_reuse_prevention      = 24
-  require_lowercase_characters   = true
-  require_numbers                = true
-  require_uppercase_characters   = true
-  require_symbols                = true
-  allow_users_to_change_password = true
+  minimum_password_length      = 16
+  max_password_age             = 90
+  password_reuse_prevention    = 24
+  require_lowercase_characters = true
+  require_numbers              = true
+  require_uppercase_characters = true
 }
