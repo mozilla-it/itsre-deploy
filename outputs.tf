6edbd8fe8197dff1b539543c909b0ef82086ccba
limed <limed@mozilla.com>
2019-04-25 15:35:37 -0700

    Output public and private subnet ids

--- outputs.tf
+++ outputs.tf
@@ -22,6 +22,14 @@ output "vpc_id" {
   value = "${module.vpc.vpc_id}"
 }
 
+output "public_subnets" {
+  value = "${module.vpc.public_subnets}"
+}
+
+output "private_subnets" {
+  value = "${module.vpc.private_subnets}"
+}
+
 output "cloudhealth_role_arn" {
   value = "${module.cloudhealth.cloudhealth_assume_role_arn}"
 }
