f4f58491c0fbd23e468810c4376d7d093fc602f2
limed <limed@mozilla.com>
2019-11-07 16:55:57 -0800

    Adding policy arn

---
+++ modules/policies/main.tf
@@ -0,0 +1,22 @@
+data "aws_iam_policy_document" "billing-readonly" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "aws-portal:ViewAccount",
+      "aws-portal:ViewBilling",
+      "aws-portal:ViewPaymentMethods",
+      "aws-portal:ViewUsage",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "billing-readonly" {
+  count       = "${var.enabled}"
+  name        = "BillingReadonly"
+  path        = "/"
+  description = "Readonly access to Account billing"
+  policy      = "${data.aws_iam_policy_document.billing-readonly.json}"
+}
---
+++ modules/policies/outputs.tf
@@ -0,0 +1,4 @@
+
+output "billing_readonly_policy_arn" {
+  value = "${aws_iam_policy.billing-readonly.arn}"
+}
